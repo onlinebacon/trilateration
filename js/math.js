3e35aeba27b0bcb31ffffafaac24fd8ca49ff4f3
onlinebacon <bacon.antife@gmail.com>
2022-04-24 04:26:24 -0300

    Code pruned

--- js/math.js
+++ js/math.js
@@ -1,7 +1,4 @@
-import { get2CirclesIntersections, circlesIntersect } from '../../jslib/sphere-trilateration.js';
-import { calcDist, coordAzDistToPoint } from '../../jslib/sphere-math.js';
-
-import coordClusteredSearch from './coord-clustered-search.js';
+import { coordAzDistToPoint } from '../../jslib/sphere-math.js';
 
 export const getCoordCircle = (lat, lon, radius, numberOfPoints = 32) => {
 	const coord = [ lat, lon ];
@@ -12,31 +9,4 @@ export const getCoordCircle = (lat, lon, radius, numberOfPoints = 32) => {
 		res.push(point);
 	}
 	return res;
-};
-
-export const getErrorFunction = (args) => {
-	const calcError = (coord) => {
-		let sum = 0;
-		for (let i=args.length; i--;) {
-			const { gp, arc } = args[i];
-			const error = calcDist(coord, gp) - arc;
-			sum += error*error;
-		}
-		return sum;
-	};
-	return calcError;
-};
-
-export const trilaterate = (args) => {
-	if (args.length === 2) {
-		const [ a, b ] = args;
-		if (circlesIntersect(a.gp, a.arc, b.gp, b.arc)) {
-			return get2CirclesIntersections(
-				a.gp, a.arc,
-				b.gp, b.arc,
-			);
-		}
-	}
-	const calcError = getErrorFunction(args);
-	return [ coordClusteredSearch({ calcError }) ];
 };
